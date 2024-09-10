4b3c6f63c8d1d76cd2eaaa996c2f41e9059ea257
Abhishek <abhishekchauhan46396@gmail.com>
2024-09-11 00:42:35 +0530

    todoapp

--- Server/Index.js
+++ Server/Index.js
@@ -3,13 +3,19 @@ const mongoose=require("mongoose")
 const bodyparser=require("body-parser")
 const cors=require("cors")
 require("dotenv").config()
+const swagger_ui=require("swagger-ui-express")
+const Outputopenapi=require("./output.openapi.json")
 
 const app=express()
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 app.use(bodyparser.json())
-
+app.use("/doc",swagger_ui.serve,swagger_ui.setup(Outputopenapi,{
+    title:"Tasks"
+}))
 const todoRouter=require("./Router/todoRoutes")
 const Port=process.env.Port
 const DB_URL=process.env.DB_URL
